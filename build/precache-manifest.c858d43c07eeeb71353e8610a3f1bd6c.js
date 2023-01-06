6d34e2505d6a4bd23749d585b0d904993c553183
Muhammad Rafi <88550428+RaxonRafi@users.noreply.github.com>
2023-01-06 15:31:32 +0600

    first commit

---
+++ build/precache-manifest.c858d43c07eeeb71353e8610a3f1bd6c.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda30e456aa49ea1f82484e6698436a9",
+    "url": "/demo/react/finsa/index.html"
+  },
+  {
+    "revision": "5199daea946c510c2c0d",
+    "url": "/demo/react/finsa/static/css/2.9d91f697.chunk.css"
+  },
+  {
+    "revision": "ab1e2ab2998a28bd11e5",
+    "url": "/demo/react/finsa/static/css/main.2f480e33.chunk.css"
+  },
+  {
+    "revision": "5199daea946c510c2c0d",
+    "url": "/demo/react/finsa/static/js/2.8c47113c.chunk.js"
+  },
+  {
+    "revision": "70deb6e51091c32b68616cac7f7475b8",
+    "url": "/demo/react/finsa/static/js/2.8c47113c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab1e2ab2998a28bd11e5",
+    "url": "/demo/react/finsa/static/js/main.108642cf.chunk.js"
+  },
+  {
+    "revision": "d645fb0a4d737125f156",
+    "url": "/demo/react/finsa/static/js/runtime-main.4b19a390.js"
+  },
+  {
+    "revision": "763a93be4af0a9ff8d168f34cc100783",
+    "url": "/demo/react/finsa/static/media/03.763a93be.png"
+  },
+  {
+    "revision": "3b8b18a7d1f007ab041c3a5796ded21c",
+    "url": "/demo/react/finsa/static/media/05.3b8b18a7.png"
+  },
+  {
+    "revision": "869017d02e2f6c7600daaeb97e6063f7",
+    "url": "/demo/react/finsa/static/media/06.869017d0.png"
+  },
+  {
+    "revision": "fdd9f3ac3a41a521cf7da75d9cffd011",
+    "url": "/demo/react/finsa/static/media/07.fdd9f3ac.png"
+  },
+  {
+    "revision": "32d111e13020bd0c9f9376dd3de8d4ec",
+    "url": "/demo/react/finsa/static/media/08.32d111e1.png"
+  },
+  {
+    "revision": "a126ec2cee915c2565c3b2cf29d13dc9",
+    "url": "/demo/react/finsa/static/media/13.a126ec2c.png"
+  },
+  {
+    "revision": "f2b96a0f04d4c658ff1e7a21ba724682",
+    "url": "/demo/react/finsa/static/media/16.f2b96a0f.png"
+  },
+  {
+    "revision": "1afb2669c4fb1b8f702024c233a110a8",
+    "url": "/demo/react/finsa/static/media/Flaticon.1afb2669.woff"
+  },
+  {
+    "revision": "37452227217b4e8a427f48042191bace",
+    "url": "/demo/react/finsa/static/media/Flaticon.37452227.svg"
+  },
+  {
+    "revision": "88d7a170ba40ff5d6340265b6dadbf36",
+    "url": "/demo/react/finsa/static/media/Flaticon.88d7a170.eot"
+  },
+  {
+    "revision": "bf9c9089e2b79c6b1a1e1302a312ffff",
+    "url": "/demo/react/finsa/static/media/Flaticon.bf9c9089.woff2"
+  },
+  {
+    "revision": "dc5556fe38ef4d27ee7096e842c06ed9",
+    "url": "/demo/react/finsa/static/media/Flaticon.dc5556fe.ttf"
+  },
+  {
+    "revision": "0605c45b241369b059cb29eb259cf910",
+    "url": "/demo/react/finsa/static/media/fa-brands-400.0605c45b.svg"
+  },
+  {
+    "revision": "3bdfbaeb74b5c7622c2fc07bfec4c174",
+    "url": "/demo/react/finsa/static/media/fa-brands-400.3bdfbaeb.eot"
+  },
+  {
+    "revision": "4005f8899c332f8a2ecfb49f567d3d5d",
+    "url": "/demo/react/finsa/static/media/fa-brands-400.4005f889.ttf"
+  },
+  {
+    "revision": "889855136f55578ad089107285632b37",
+    "url": "/demo/react/finsa/static/media/fa-brands-400.88985513.woff"
+  },
+  {
+    "revision": "dc83ddbbdd108b159117928063699c4e",
+    "url": "/demo/react/finsa/static/media/fa-brands-400.dc83ddbb.woff2"
+  },
+  {
+    "revision": "1b7d95302d7cbc6a87a773c9d5be58e8",
+    "url": "/demo/react/finsa/static/media/fa-light-300.1b7d9530.ttf"
+  },
+  {
+    "revision": "32291ebb691b2f787dbde9130e3ca1c9",
+    "url": "/demo/react/finsa/static/media/fa-light-300.32291ebb.svg"
+  },
+  {
+    "revision": "6b1c4bcb2a11d69fc64a61f889dfa1d3",
+    "url": "/demo/react/finsa/static/media/fa-light-300.6b1c4bcb.woff2"
+  },
+  {
+    "revision": "a726cedb85b1cfd642eefdf111dec9bf",
+    "url": "/demo/react/finsa/static/media/fa-light-300.a726cedb.woff"
+  },
+  {
+    "revision": "c72987d083289070df1a0f1075cf4061",
+    "url": "/demo/react/finsa/static/media/fa-light-300.c72987d0.eot"
+  },
+  {
+    "revision": "23458a3f7f740a5490c3937006e6b3fd",
+    "url": "/demo/react/finsa/static/media/fa-regular-400.23458a3f.woff"
+  },
+  {
+    "revision": "3593cd9504bc12c2a3bc2ae200d0857a",
+    "url": "/demo/react/finsa/static/media/fa-regular-400.3593cd95.eot"
+  },
+  {
+    "revision": "935ae23ba86e111411100dca037d2f0a",
+    "url": "/demo/react/finsa/static/media/fa-regular-400.935ae23b.woff2"
+  },
+  {
+    "revision": "b968df14569fcda0208ce05640e71195",
+    "url": "/demo/react/finsa/static/media/fa-regular-400.b968df14.svg"
+  },
+  {
+    "revision": "e60701577a5df7b92175e2eced183e6e",
+    "url": "/demo/react/finsa/static/media/fa-regular-400.e6070157.ttf"
+  },
+  {
+    "revision": "525bd4ae5b9eaa1e23b4a5c8b28c0d9f",
+    "url": "/demo/react/finsa/static/media/fa-solid-900.525bd4ae.woff2"
+  },
+  {
+    "revision": "6628fa0a2a8a468066e2c7671fb66c86",
+    "url": "/demo/react/finsa/static/media/fa-solid-900.6628fa0a.eot"
+  },
+  {
+    "revision": "7a24cacaa305b1ca642b878e5ca8f541",
+    "url": "/demo/react/finsa/static/media/fa-solid-900.7a24caca.ttf"
+  },
+  {
+    "revision": "ae249d48f7cde3656ea6c710ced852fe",
+    "url": "/demo/react/finsa/static/media/fa-solid-900.ae249d48.woff"
+  },
+  {
+    "revision": "d4e4b058b0a1fd4afb326ae7c7fd5c09",
+    "url": "/demo/react/finsa/static/media/fa-solid-900.d4e4b058.svg"
+  },
+  {
+    "revision": "8f742c21865f932a73b165c0cb612d82",
+    "url": "/demo/react/finsa/static/media/play.8f742c21.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/demo/react/finsa/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/demo/react/finsa/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/demo/react/finsa/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/demo/react/finsa/static/media/slick.f97e3bbf.svg"
+  }
+]);
